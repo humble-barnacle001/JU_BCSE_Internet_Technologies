2cfcba347b3150926be907b18e7a7a53a8f8a96e
Rwitick Ghosh <58736240+humble-barnacle001@users.noreply.github.com>
2022-01-21 00:45:47 +0530

    Optimise workflow

--- runner.sh
+++ runner.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build/$1
 cp -r $1/img build/$1/img
-cp $1/README.md build/$1/README.md
+sed '3s/^/[View Report](Report.pdf)\n\n/' $1/README.md > build/$1/README.md
 cp $1/README.pdf build/$1/Report.pdf
